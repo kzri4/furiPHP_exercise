997dbc1b9a8fe2a2453d97118e9a1794d33e6b68
kondo <kazu4126ab@gmail.com>
2020-09-29 06:55:29 +0900

    修正済みsecond

--- chap3_3.php
+++ chap3_3.php
@@ -2,12 +2,12 @@
 
 foreach(range(1,100) as $int){
     if($int%3==0 && $int%5==0){
-        echo("FizzBuzz");}
-    elseif($int%3==0){
-        echo("Fizz");}
-    elseif($int%5==0){
-        echo("Buzz");}
-        else{
+        echo("FizzBuzz");
+    }elseif($int%3==0){
+        echo("Fizz");
+    }elseif($int%5==0){
+        echo("Buzz");
+    }else{
         echo($int);
     }
 }
